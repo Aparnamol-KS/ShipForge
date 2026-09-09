7b69c8a87c10c3ae0055065b26e168dfd504f5df
Aparna <aparnamolks.off@gmail.com>
2026-09-10 00:36:02 +0530

    added docker file and created postgresql connection

---
+++ backend/app/database/models.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timezone
+
+from app.database.connection import Base
+from sqlalchemy import DateTime, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(
+        primary_key=True,
+        autoincrement=True,
+    )
+
+    name: Mapped[str] = mapped_column(
+        String(100),
+        nullable=False,
+    )
+
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True,
+    )
+
+    repository_url: Mapped[str | None] = mapped_column(
+        String(500),
+        nullable=True,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+    )
+
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
+from app.database.connection import Base, engine
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from app.database.models import Project
 
 app = FastAPI(title="ShipForge API")
 
+Base.metadata.create_all(bind=engine)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173"],
@@ -14,13 +17,9 @@ app.add_middleware(
 
 @app.get("/")
 def root():
-    return {
-        "message": "ShipForge API is running"
-    }
+    return {"message": "ShipForge API is running"}
 
 
 @app.get("/health")
 def health_check():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
