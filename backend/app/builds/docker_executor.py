f7a6fce58747f42522b62c34b6fdf17a5f521e35
Aparna <aparnamolks.off@gmail.com>
2026-09-25 13:28:05 +0530

    fix

--- backend/app/builds/docker_executor.py
+++ backend/app/builds/docker_executor.py
@@ -14,6 +14,8 @@ def run_command(
         "docker",
         "run",
         "--rm",
+        "--volume",
+        "/var/run/docker.sock:/var/run/docker.sock",
         "--network",
         BUILD_NETWORK,
         "-e",
